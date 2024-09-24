e4e8addd10708676c67621e704d18d9594a2d0d0
Gabriel <leirbagaboo@gmail.com>
2024-09-23 22:47:48 -0400

    ui design complete

--- src/Pages/FavoritePage.jsx
+++ src/Pages/FavoritePage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import RecipeCard from "../components/RecipeCard";
+const fav = false;
 
 const FavoritePage = () => {
-  return <div>FavoritePage</div>;
+  return (
+    <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen">
+      <div className="max-w-screen-lg mx-auto">
+        <p className="font-bold text-3xl md:text-5xl my4">My favorites</p>
+        {!fav && (
+          <div className="h-[80vh] flex flex-col items-center gap-4">
+            <img src="/404.svg" className="h-3/4" alt="404" />
+          </div>
+        )}
+        {fav && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <RecipeCard />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FavoritePage;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,25 +1,40 @@
-import { Search } from 'lucide-react'
-import React from 'react'
+import { Search } from "lucide-react";
+import React from "react";
+import RecipeCard from "../components/RecipeCard";
 
 const HomePage = () => {
   return (
-    <div className='bg-[#faf9fb] p-10 flex-1'>
+    <div className="bg-[#faf9fb] p-10 flex-1">
       <div className="max-w-screen-lg mx-auto">
         <form>
           <label htmlFor="" className="input shadow-md flex items-center gap-2">
-            <Search size={"24"}/>
-            <input type="text" className='text-sm md:text-md grow' placeholder='Que quieres buscar?'/>
+            <Search size={"24"} />
+            <input
+              type="text"
+              className="text-sm md:text-md grow"
+              placeholder="Que quieres buscar?"
+            />
           </label>
         </form>
         <h1 className="font-bold text-3xl md:text-5xl mt-4">Recomendado</h1>
-        <p className='text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight'>Popular</p>
+        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
+          Popular
+        </p>
 
-        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"></div>
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {/* first recipe*/}
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+          <RecipeCard />
+        </div>
       </div>
-
     </div>
+  );
+};
 
-  )
-}
-
-export default HomePage
+export default HomePage;
